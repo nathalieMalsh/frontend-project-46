d96f602da646abac14486aafe8986ed7422031cc
nmalysheva99@mail.ru <nathalie_malsh@LAPTOP-HHAP2AP9>
2024-02-27 19:09:39 +0300

    add lint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,5 +5,7 @@ import { program } from 'commander';
 program
   .version('output the version number')
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
